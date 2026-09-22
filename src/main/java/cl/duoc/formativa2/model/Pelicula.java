86b19f0be64b1e67984cfab4dff98d6a9b3bf6cd
Dizz7 <162482518+Dizz7@users.noreply.github.com>
2026-09-22 20:20:38 -0300

    Ajustes de dependencia y código
    Se agrega la dependencia: spring-boot-starter-test
    
    Se eliminan import que no se usaron

--- src/main/java/cl/duoc/formativa2/model/Pelicula.java
+++ src/main/java/cl/duoc/formativa2/model/Pelicula.java
@@ -5,8 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Table;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 
