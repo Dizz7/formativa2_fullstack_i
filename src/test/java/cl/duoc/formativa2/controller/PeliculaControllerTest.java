a50c9280d0c9ed2f8f6ea9614dbada1b651addc7
Dizz7 <162482518+Dizz7@users.noreply.github.com>
2026-09-23 13:01:55 -0300

    Prueba Unitaria Modelo Pelicula
    Se agrega prueba unitaria de Getters y Setters para modelo Pelicula.java

---
+++ src/test/java/cl/duoc/formativa2/controller/PeliculaControllerTest.java
@@ -0,0 +1,5 @@
+package cl.duoc.formativa2.controller;
+
+public class PeliculaControllerTest {
+    
+}
---
+++ src/test/java/cl/duoc/formativa2/model/PeliculaModelTest.java
@@ -0,0 +1,26 @@
+package cl.duoc.formativa2.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PeliculaModelTest {
+    @Test
+    void testGettersAndSetters() {
+        Pelicula pelicula = new Pelicula();
+        pelicula.setId(1L);
+        pelicula.setTitulo("Inception");
+        pelicula.setAño(2010);
+        pelicula.setDirector("Christopher Nolan");
+        pelicula.setGenero("Sci-Fi");
+        pelicula.setSinopsis("A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.");
+
+        assertEquals(1, pelicula.getId());
+        assertEquals("Inception", pelicula.getTitulo());
+        assertEquals(2010, pelicula.getAño());
+        assertEquals("Christopher Nolan", pelicula.getDirector());
+        assertEquals("Sci-Fi", pelicula.getGenero());
+        assertEquals("A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.", pelicula.getSinopsis());
+    }
+    
+
+}
